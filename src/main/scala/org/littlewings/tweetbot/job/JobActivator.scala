d16e5e919c118581e3bb9d431b5efc12c2e89fd4
kazuhira <kazuhira.m@gmail.com>
2017-01-05 01:09:32 +0900

    refactor JobActivator

--- src/main/scala/org/littlewings/tweetbot/job/JobActivator.scala
+++ src/main/scala/org/littlewings/tweetbot/job/JobActivator.scala
@@ -1,21 +1,17 @@
 package org.littlewings.tweetbot.job
 
 import javax.enterprise.inject.spi.{Bean, CDI}
-import javax.inject.Inject
 import javax.servlet.annotation.WebListener
 import javax.servlet.{ServletContextEvent, ServletContextListener}
 
 import org.apache.deltaspike.scheduler.spi.Scheduler
+import org.littlewings.tweetbot.LoggerSupport
 import org.quartz.Job
-import org.slf4j.Logger
 
 import scala.collection.JavaConverters._
 
 @WebListener
-class JobActivator extends ServletContextListener {
-  @Inject
-  private[job] var logger: Logger = _
-
+class JobActivator extends ServletContextListener with LoggerSupport {
   override def contextInitialized(sce: ServletContextEvent): Unit = {
     val scheduler = CDI.current.select(classOf[Scheduler[Job]]).get
 
