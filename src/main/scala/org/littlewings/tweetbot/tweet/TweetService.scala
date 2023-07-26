900bab2acd42615caeccbf60101499b8a5377a35
kazuhira-r <kazuhira.m@gmail.com>
2023-07-27 00:56:40 +0900

    migrate, Twitter v2 API

--- src/main/scala/org/littlewings/tweetbot/tweet/TweetService.scala
+++ src/main/scala/org/littlewings/tweetbot/tweet/TweetService.scala
@@ -1,10 +1,24 @@
 package org.littlewings.tweetbot.tweet
 
 import twitter4j.Twitter
+import twitter4j.TwitterV2ExKt
 
 trait TweetService {
   protected val twitter: Twitter
 
   def tweet(source: TweetSource): Unit =
-    twitter.updateStatus(source.format)
+    TwitterV2ExKt.getV2(twitter).createTweet(
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      source.format
+    )
 }
